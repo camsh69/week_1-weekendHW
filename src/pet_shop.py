7e32c478f04f9fb479b324ba7fc1ceef381234e0
Campbell Forsyth <camsh69@gmail.com>
2021-11-19 13:29:56 +0000

    second commit

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -12,4 +12,18 @@ def add_or_remove_cash(pet_shop, amount):
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
+def increase_pets_sold(pet_shop, num_pets_sold):
+    new_num_sold = get_pets_sold(pet_shop) + num_pets_sold
+    pet_shop["admin"]["pets_sold"] = new_num_sold
 
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    same_breed = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            same_breed.append(pet["breed"])
+
+    return same_breed
+    
