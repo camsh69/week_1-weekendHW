37ed27c4e3b3c410136c1a4072296980b7c886ba
Campbell Forsyth <camsh69@gmail.com>
2021-11-19 15:20:12 +0000

    fourth commit - finished non-optional HW

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -30,7 +30,7 @@ def get_pets_by_breed(pet_shop, breed):
 def find_pet_by_name(pet_shop, name):
     for pet in pet_shop["pets"]:
         if pet["name"] == name:
-            return pet["name"]
+            return pet
 
 def remove_pet_by_name(pet_shop, name):
     i = 0
@@ -42,3 +42,15 @@ def remove_pet_by_name(pet_shop, name):
 def add_pet_to_stock(pet_shop, new_pet):
     pet_shop["pets"].append(new_pet)
     
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, amount):
+    new_cash_amt = get_customer_cash(customer) - amount
+    customer["cash"] = new_cash_amt
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
